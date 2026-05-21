d8b0e1a95c4138d69b1e7a4fdc38057d14b263c2
Tsubasa Watanabe <t.watanabe423@gmail.com>
2026-05-21 06:41:24 +0000

    fix build bug lazy import

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,14 @@
 import sql from "mssql";
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
-}
-
 let poolPromise: Promise<sql.ConnectionPool> | undefined;
 
 export function getDbPool(): Promise<sql.ConnectionPool> {
+  const connectionString = process.env.DATABASE_URL;
+  
+  if (!connectionString) {
+    throw new Error("DATABASE_URL is not set");
+  }
+ 
   if (!poolPromise) {
     poolPromise = sql.connect(connectionString!);
   }
