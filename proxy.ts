9d34d821ee8c7bce3a37f95f84c0e8952455052c
Tsubasa Watanabe <t.watanabe423@gmail.com>
2026-05-31 03:33:17 +0000

    fix image expression

--- proxy.ts
+++ proxy.ts
@@ -23,5 +23,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|mock/|generated-frames/).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|mock/|generated-frames/|goodjob/).*)"],
 };
